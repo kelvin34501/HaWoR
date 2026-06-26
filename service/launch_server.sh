0a8190242947fa35ce1e908d4dd9231e0f24b318
kelvin34501 <kelvin34501@foxmail.com>
2026-06-26 20:42:08 +0800

    mod: respect envvar

--- service/launch_server.sh
+++ service/launch_server.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 export HAWOR_CACHE_DIR=./example/hawor_process
-export HAWOR_GPU_IDS=0
+export HAWOR_GPU_IDS=$CUDA_VISIBLE_DEVICES
 export HAWOR_JOBS_PER_GPU=4   # max concurrent jobs per GPU (default 1)
 export HAWOR_CLEANUP_INTERMEDIATE=true
 export HAWOR_CLEANUP_FAILED_CACHE=false
 export HAWOR_COPY_EXTRACTED_IMAGES=false
-export HAWOR_S3MOUNT_BIN=/mnt/petrelfs/share/s3mount
+export HAWOR_S3MOUNT_BIN=/mnt/petrelfs/zhanxinyu/software/s3mount
 export HAWOR_MOUNT_ROOT=/tmp/hawor_service/runtime/
 export HAWOR_MOUNT_READY_TIMEOUT=30
 python -m service.api_server
